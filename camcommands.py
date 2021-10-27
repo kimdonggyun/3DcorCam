ce6c91541bc676688d51eca43d4a6858507ac604
kimdonggyun <kdk921219@gamil.com>
2021-10-27 22:40:02 +0200

    fps fixed, recording workgin.. at least

--- camcommands.py
+++ camcommands.py
@@ -6,6 +6,7 @@ import os
 import sys
 from pypylon import genicam
 from pypylon import pylon
+import datetime
 
 def camOpen(camera):
     try:
@@ -14,3 +15,22 @@ def camOpen(camera):
         return camera
     except:
         return 0
+
+def initiate_and_setup_cam(fps=24):
+    # enable emulation 
+    import os
+    os.environ["PYLON_CAMEMU"] = "1"
+
+    cam = pylon.InstantCamera(pylon.TlFactory.GetInstance().CreateFirstDevice())
+    cam.Open()
+    cam.ImageFilename = img_dir
+    cam.ImageFileMode = "On" # enable image file test pattern
+    cam.TestImageSelector = "Off" # disable testpattern [ image file is "real-image"]
+    cam.PixelFormat = "Mono8" # choose one pixel format. camera emulation does conversion on the fly
+
+    cam.Height = height
+    cam.Width = width
+    
+    cam.AcquisitionFrameRateAbs.SetValue(fps);
+    
+    return cam
