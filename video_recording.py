2ac19beeb859d2b28094a58ad3a14900e2d0b852
kimdonggyun <kdk921219@gamil.com>
2021-11-04 12:25:20 +0100

    a script for video recording created

---
+++ video_recording.py
@@ -0,0 +1,21 @@
+# script for video recording
+
+from pypylon import pylon
+from imageio import get_writer
+import os
+from datetime import datetime
+
+def video_recording(file_dir, filename, caminstance):
+    with get_writer(os.path.join(file_dir, filename)) as writer:
+        print("parent process : %s / process id %s" % (os.getppid(), os.getpid()))
+        print("recording start with %s at %s" % (caminstance.DeviceInfo.GetFriendlyName(), datetime.now()))
+        caminstance.StopGrabbing()
+        caminstance.StartGrabbingMax(50)
+        while caminstance.IsGrabbing():
+            try :
+                res = caminstance.RetrieveResult(10000, pylon.TimeoutHandling_ThrowException)
+            except:
+                print("something wrong while recording")
+            writer.append_data(res.Array)
+            res.Release()
+        print("recording finish with %s at %s" % (caminstance.DeviceInfo.GetFriendlyName(), datetime.now()))
