6e183b7fda7805d8b21d36c782da0826895d356e
kimdonggyun <kdk921219@gamil.com>
2021-11-04 12:07:41 +0100

    def dev info, cam init, dev_2_array created

--- camcommands.py
+++ camcommands.py
@@ -16,6 +16,14 @@ def camOpen(camera):
     except:
         return 0
 
+def cam_init ():
+    # getting instance of basler cam
+    # return devices connected
+    tl_factory = pylon.TlFactory.GetInstance()
+    devices = tl_factory.EnumerateDevices()
+    return devices
+
+
 def cam_non_identical (devices, dev_number):
     """
     Basler's package have a problem with MacOS and sometimes all connected camera doesn't show up.
@@ -23,6 +31,7 @@ def cam_non_identical (devices, dev_number):
     Therefore, no duplicating camera instant.
     This function will be running untill you get certain number of camera instant you desire
     devices = initial camera instant
+    dev_number = number of devices connected
     """
     temp_dev_list = [] # create empty camera list to com
     if len(devices) == 0:
@@ -42,6 +51,23 @@ def cam_non_identical (devices, dev_number):
                 break
     return devices
 
+def dev_info(devices):
+    # print out all connected cameras' basic information
+    # 2. check the devices information
+    if len(devices) == 0:
+        raise pylon.RuntimeException("No camera connected")
+    else:
+        print("total number of devices:", len(devices)) # print total number of devices connected
+        for i, dev_info in enumerate(devices): # 
+            if dev_info.GetDeviceClass() == 'BaslerGigE': # check if the connected cameras are GigE model. Otherwise software trigger won't work
+                print("using %s as IP %s SN %s" % (dev_info.GetModelName(), dev_info.GetIpAddress() ,dev_info.GetSerialNumber()))
+            else:
+                raise EnvironmentError("no GigE device found")
 
-def cam_init ():
-    # 
+
+def dev_2_array (devices, dev_number):
+    # create array of devices connected for the easier handling of individual camera
+    cams = pylon.InstantCameraArray(min(len(devices), dev_number)) # create instant
+    for i, cam in enumerate(cams):
+        cam.Attach(tl_factory.CreateDevice(devices[i]))
+
