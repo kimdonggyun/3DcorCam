8cfef76a4e449dd3b395ed81a43c22a1c2e2f303
kimdonggyun <kdk921219@gamil.com>
2021-11-04 11:58:37 +0100

    def cam_non_identical created

--- camcommands.py
+++ camcommands.py
@@ -16,21 +16,32 @@ def camOpen(camera):
     except:
         return 0
 
-def initiate_and_setup_cam(fps=24):
-    # enable emulation 
-    import os
-    os.environ["PYLON_CAMEMU"] = "1"
+def cam_non_identical (devices, dev_number):
+    """
+    Basler's package have a problem with MacOS and sometimes all connected camera doesn't show up.
+    This def is created to check whether call camera's instants are created.
+    Therefore, no duplicating camera instant.
+    This function will be running untill you get certain number of camera instant you desire
+    devices = initial camera instant
+    """
+    temp_dev_list = [] # create empty camera list to com
+    if len(devices) == 0:
+        raise pylon.RuntimeException("No camera connected, please check the camera connection!")
+    else:
+        for dev_info in devices:
+            temp_dev_list.append(dev_info.GetSerialNumber())
+        while len(set(temp_dev_list)) != dev_number: # count the unique camera instant and compare it with desired number of cameras
+            tl_factory = pylon.TlFactory.GetInstance() # get instance again
+            devices = tl_factory.EnumerateDevices()
+            temp_dev_list = []
+            for dev_info in devices:
+                temp_dev_list.append(dev_info.GetSerialNumber())
+            print(temp_dev_list)
+            if len(set(temp_dev_list)) == dev_number:
+                print("all cameras' instance are created", temp_dev_list)
+                break
+    return devices
 
-    cam = pylon.InstantCamera(pylon.TlFactory.GetInstance().CreateFirstDevice())
-    cam.Open()
-    cam.ImageFilename = img_dir
-    cam.ImageFileMode = "On" # enable image file test pattern
-    cam.TestImageSelector = "Off" # disable testpattern [ image file is "real-image"]
-    cam.PixelFormat = "Mono8" # choose one pixel format. camera emulation does conversion on the fly
 
-    cam.Height = height
-    cam.Width = width
-    
-    cam.AcquisitionFrameRateAbs.SetValue(fps);
-    
-    return cam
+def cam_init ():
+    # 
