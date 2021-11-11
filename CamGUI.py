046bb76bc15d1a0dddcf63d1f9a046de9c61dfda
kimdonggyun <kdk921219@gmail.com>
2021-11-11 16:40:23 +0100

    improt functions

--- CamGUI.py
+++ CamGUI.py
@@ -1,1 +1,12 @@
 # GUI for camera control
+# created by Dong-gyun Kim 
+# contact dong-gyun.kim@awi.de
+
+#import functions
+import tkinter as tk
+from tkinter import ttk
+from camcommands import cam_init, dev_set_param
+from video_recording import video_recording
+
+
+
